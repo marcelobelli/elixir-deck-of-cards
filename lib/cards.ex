9ffd49a08b59ed79064e8e778796b0d35d99fb76
Marcelo Belli <marcelo@belli.me>
2018-06-24 18:03:52 -0300

    Cleanup

--- lib/cards.ex
+++ lib/cards.ex
@@ -3,15 +3,6 @@ defmodule Cards do
     values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    # First solution
-    # cards = for suit <- suits do
-    #   for value <- values do
-    #     "#{value} of #{suit}"
-    #   end
-    # end
-    # List.flatten(cards)
-
-    # Better solution
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
@@ -34,17 +25,6 @@ defmodule Cards do
     File.write(filename, binary)
   end
 
-  # Primeiro caso
-  # def load(filename) do
-  #   {status, binary} = File.read(filename)
-
-  #   case status do
-  #     :ok -> :erlang.binary_to_term(binary)
-  #     :error -> "File not found"
-  #   end
-  # end
-
-  # Mais clean
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
